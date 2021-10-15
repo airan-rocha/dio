f95cb9e3d3aba35d5a36592b17d53d53e7d57180
airan-rocha <rocha.airan@gmail.com>
2021-10-15 07:28:35 -0300

    js objetos

---
+++ curso-javascript/script3.js
@@ -0,0 +1,33 @@
+// var pessoas = ["Airan", "Valdirene"];
+
+// pessoas.men = "menino";
+
+// console.log(pessoas);
+
+// console.log("men" in pessoas);
+
+// var carro = {marca: "Honda", modelo: "Civic"};
+
+// console.log(carro);
+
+// console.log("marca" in carro);
+
+//
+
+function Animal(qtdePatas) {
+    this.qtdePatas = qtdePatas;
+}
+
+function Cachorro(dono){
+    this.late = "au au";
+    this.dono = dono;
+}
+
+
+const bulldog = new Cachorro('eu');
+
+console.log(bulldog);
+// console.log(bulldog);
+
+// console.log("qtdePatas" in cachorro);
+
