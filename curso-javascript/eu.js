a86ec2a160682d188eb6e9e9699ddd51647834bb
airan-rocha <rocha.airan@gmail.com>
2021-10-19 14:58:37 -0300

    trabalhando com classes

--- curso-javascript/eu.js
+++ curso-javascript/eu.js
@@ -1,19 +1,59 @@
 class oi {
     constructor(msg){
-        this.mensagem = msg;
+        this.msg = msg;
     }
 
     set mensagem(msg){
-        this.mensagem = msg;
+        this.msg = msg;
     }
 
     get mensagem(){
-        return this.mensagem;
+        return this.msg;
     }
 }
 
 var ee = new oi();
 
-ee.mensagem("Airan Rocha");
+ee.mensagem = "Airan Rocha";
 
-console.log(ee.mensagem());
+console.log(ee.mensagem);
+
+// *************************
+
+class Retangulo {
+    constructor(altura, largura) {
+      this.altura = altura; this.largura = largura;
+    }
+  //Getter
+    get area() {
+        return this.calculaArea()
+    }
+
+    calculaArea() {
+        return this.altura * this.largura;
+    }
+}
+
+const quadrado = new Retangulo(10, 10);
+
+console.log(quadrado.area);
+
+// ###########################3
+
+class Pessoa {
+    constructor(cor){
+        this.cor = cor;
+    }
+    cor(cor){
+        this.cor = cor;
+    }
+    cor(){
+        return this.cor
+    }
+}
+
+var eu = new Pessoa();
+
+eu.cor = "White";
+
+console.log(`A cor é ${eu.cor}`);
