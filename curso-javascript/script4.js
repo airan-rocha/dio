4bc3c0ac2d0d901922ba6216d0099e9890f942ad
airan-rocha <rocha.airan@gmail.com>
2021-10-21 11:00:26 -0300

    iniciando com arrow functions

---
+++ curso-javascript/script4.js
@@ -0,0 +1,18 @@
+// var log = (log) => {
+//     console.log(log);
+// };
+
+log("testando");
+
+var a = 15;
+var b = 8;
+
+var sum = (a, b) => a + b;
+var sub = (a, b) => a - b;
+
+log(`soma: ${a}+${b}=${sum(a, b)}`);
+log(`subtração: ${a}-${b}=${sub(a, b)}`);
+
+function log(log) {
+    console.log(log);
+};
